61f498e84a4a986216d36f1b5f034e99dbf42bfa
Priyank Rawat <priyankrawat2001@gmail.com>
2024-04-04 08:58:27 +0000

    Final

--- models/orders.sql
+++ models/orders.sql
@@ -1,12 +1,13 @@
 {{
     config(
-    materialized="incremental")
+    materialized="incremental",
+    unique_key = 'order_id')
 }}
 
 with orders as (
     select * from {{ref("stg_orders")}}
     {% if is_incremental() %}
-    where order_date >= (select max(order_date) from {{ this }})
+    where order_date >= (select dateadd('day', -3, max(order_date)) from {{ this }})
     {% endif %}
 )
 
